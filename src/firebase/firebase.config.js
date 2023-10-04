fd20af73833595671c2a4a64f7b4ebfe9c782a14
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-10-04 17:49:28 +0600

    Height firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDybP75pb0z327GMsnzwJkf2aAiF_Uwlgs",
-    authDomain: "practic-8bfd9.firebaseapp.com",
-    projectId: "practic-8bfd9",
-    storageBucket: "practic-8bfd9.appspot.com",
-    messagingSenderId: "596465714213",
-    appId: "1:596465714213:web:e82d6458ea078f41916bcc"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_API_ID
 };
 
 const app = initializeApp(firebaseConfig);
