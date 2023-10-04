4832ea26a36a6131071c8f5a260f0f531b428873
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-10-04 18:23:53 +0600

    Photo url required delet

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -74,7 +74,7 @@ const Registration = () => {
 
                                 <div>
                                     <label for="photo" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Photo URl</label>
-                                    <input type="text" name="photo" id="photo" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Photo URL" required={true} />
+                                    <input type="text" name="photo" id="photo" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Photo URL" />
                                 </div>
 
                                 <div>
